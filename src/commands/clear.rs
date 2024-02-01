d210a272dfc7ebf17a2158b5c8be89b4ecc3f8c1
aml <andrewmloop@gmail.com>
2024-02-01 13:30:21 -0500

    Clean up comments

--- src/commands/clear.rs
+++ src/commands/clear.rs
@@ -33,49 +33,6 @@ pub async fn run(ctx: &Context, command: &ApplicationCommandInteraction) -> Comm
 
         handler.queue().stop();
 
-        // Attempt to skip the currently playing song
-        //     let skip_result = match handler.queue().stop() {
-        //         Some(track) => track.stop(),
-        //         None => {
-        //             response = CommandResponse::new()
-        //                 .description(String::from("There is no song currently playing!"))
-        //                 .color(Color::DARK_RED)
-        //                 .clone();
-
-        //             return response;
-        //         }
-        //     };
-
-        //     match skip_result {
-        //         // The song was successfully skipped. Notify the channel if the
-        //         // queue is now empty
-        //         Ok(_) => {
-        //             // We had to utilize the length of the queue before the skip
-        //             // was attempted or else we have to do some weird sequence of
-        //             // commands to ensure the queue is properly updated. Skipping/
-        //             // stopping a track takes a while to remove it from the queue
-        //             // also, manually removing tracks doesn't trigger the next
-        //             // track to play
-        //             if queue_length == 1 {
-        //                 response = CommandResponse::new()
-        //                     .description(String::from("The queue has **ended!**"))
-        //                     .color(Color::DARK_GREEN)
-        //                     .clone();
-        //             } else {
-        //                 response = CommandResponse::new()
-        //                     .description(String::from("Song **skipped!**"))
-        //                     .color(Color::DARK_GREEN)
-        //                     .clone();
-        //             }
-        //         }
-        //         Err(why) => {
-        //             println!("Error skipping track: {why}");
-        //             response = CommandResponse::new()
-        //                 .description(String::from("Error skipping song!"))
-        //                 .color(Color::DARK_RED)
-        //                 .clone();
-        //         }
-        //     };
         response = CommandResponse::new()
             .description(String::from("Queue **cleared!**"))
             .color(Color::DARK_GREEN)
