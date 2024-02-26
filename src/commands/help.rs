e8034ce57159fc70974376e09c290daa84f5fba7
aml <andrewmloop@gmail.com>
2024-02-26 16:18:59 -0500

    Update play command option names

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -22,10 +22,10 @@ pub async fn run(ctx: &Context, command: &ApplicationCommandInteraction) {
     \n**6. /loop**Enable/disable looping of the current song
     \n**7. /pause**Pause the currently playing song
     \n**8. /ping**Respond with Pong!
-    \n**9a. /play [url]**Play the audio from a Youtube video or playlist URL
-    \n**9b. /play [title]**Play the audio from a Youtube video best matching the title
-    \n**10. /resume**Resume the currently paused song
-    \n**11. /skip**Skip the currently playing song",
+    \n**9. /play [url]**Play the audio from a Youtube video or playlist URL
+    \n**10. /play [title]**Play the audio from a Youtube video best matching the title
+    \n**11. /resume**Resume the currently paused song
+    \n**12. /skip**Skip the currently playing song",
     );
 
     response_embed
